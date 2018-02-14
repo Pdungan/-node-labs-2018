03820c368ddbea81e827694f488d498325adc3b8
Pdungan <pauldungan2@gmail.com>
2018-02-14 12:50:41 +0000

    added routing for add,update,delete contact

--- node-labe1/api/contacts/index.js
+++ node-labe1/api/contacts/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {contacts} from 'contacts';
+import {contacts} from './contacts';
 
 const router = express.Router(); // eslint-disable-line
 router.get('/', (req, res) => {
